37bc8a585ffd010a786c455cbfc0eede5b76e385
BrytAdjei <nanaadjeibright1@gmail.comgit config --global user.email nanaadjeibright1@gmail.com>
2023-05-02 14:42:10 +0000

    first commit

---
+++ pms_db.sql
@@ -0,0 +1,107 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 01, 2023 at 09:05 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pms_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `action_list`
+--
+-- Error reading structure for table pms_db.action_list: #1932 - Table &#039;pms_db.action_list&#039; doesn&#039;t exist in engine
+-- Error reading data for table pms_db.action_list: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `pms_db`.`action_list`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cell_list`
+--
+-- Error reading structure for table pms_db.cell_list: #1932 - Table &#039;pms_db.cell_list&#039; doesn&#039;t exist in engine
+-- Error reading data for table pms_db.cell_list: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `pms_db`.`cell_list`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `crime_list`
+--
+-- Error reading structure for table pms_db.crime_list: #1932 - Table &#039;pms_db.crime_list&#039; doesn&#039;t exist in engine
+-- Error reading data for table pms_db.crime_list: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `pms_db`.`crime_list`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inmate_crimes`
+--
+-- Error reading structure for table pms_db.inmate_crimes: #1932 - Table &#039;pms_db.inmate_crimes&#039; doesn&#039;t exist in engine
+-- Error reading data for table pms_db.inmate_crimes: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `pms_db`.`inmate_crimes`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inmate_list`
+--
+-- Error reading structure for table pms_db.inmate_list: #1932 - Table &#039;pms_db.inmate_list&#039; doesn&#039;t exist in engine
+-- Error reading data for table pms_db.inmate_list: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `pms_db`.`inmate_list`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `prison_list`
+--
+-- Error reading structure for table pms_db.prison_list: #1932 - Table &#039;pms_db.prison_list&#039; doesn&#039;t exist in engine
+-- Error reading data for table pms_db.prison_list: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `pms_db`.`prison_list`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `record_list`
+--
+-- Error reading structure for table pms_db.record_list: #1932 - Table &#039;pms_db.record_list&#039; doesn&#039;t exist in engine
+-- Error reading data for table pms_db.record_list: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `pms_db`.`record_list`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `system_info`
+--
+-- Error reading structure for table pms_db.system_info: #1932 - Table &#039;pms_db.system_info&#039; doesn&#039;t exist in engine
+-- Error reading data for table pms_db.system_info: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `pms_db`.`system_info`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table pms_db.users: #1932 - Table &#039;pms_db.users&#039; doesn&#039;t exist in engine
+-- Error reading data for table pms_db.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `pms_db`.`users`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `visit_list`
+--
+-- Error reading structure for table pms_db.visit_list: #1932 - Table &#039;pms_db.visit_list&#039; doesn&#039;t exist in engine
+-- Error reading data for table pms_db.visit_list: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `pms_db`.`visit_list`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
